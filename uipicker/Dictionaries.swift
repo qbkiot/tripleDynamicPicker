494393a4352cb8da6ea0ce5ef4d9f6ed5fe2e868
qbk <qbkiot@gmail.com>
2020-02-02 22:52:37 +0100

    second issue - adku comments

---
+++ uipicker/Dictionaries.swift
@@ -0,0 +1,13 @@
+//
+//  Dictionaries.swift
+//  uipicker
+//
+//  Created by Jakub Opała on 02/02/2020.
+//  Copyright © 2020 Jakub Opała. All rights reserved.
+//
+
+import Foundation
+
+struct Dictionaries {
+    var carDictionary = ["car": ["fiesta": ["year": ["1990", "2002"]], "focus": ["year": ["2020", "2002"]]], "car2": ["reno": ["year": ["1992", "2002"]], "super": ["year": ["1993", "1994"]]]]
+}
--- uipicker/ViewController.swift
+++ uipicker/ViewController.swift
@@ -8,53 +8,57 @@
 
 import UIKit
 
-class CarPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var makePicker: UIPickerView!
     @IBOutlet weak var modelPicker: UIPickerView!
     @IBOutlet weak var yearPicker: UIPickerView!
     
-    
+    //ilość UIPickerView, poniżej zadeklarowane jako Dictionary
     var listOfMakes: [String] = [String]()
-    let listOfModels: [String] = [String]()
-    let listOfYears: [String] = [String]()
-    var carList: Dictionary <String, Dictionary<String,Dictionary<String,String>>> = Dictionary()
-    //var carList: Dictionary <String, Dictionary<String,Dictionary<String,String>>> = Dictionary()
-    //var carDictionary = { make1: {model1: {year1, year2}, model2: {year1, year2}}, make2: {model4: {year16, year23}, model5: {year21, year23}}}
-    //var carDictionary = ["ford": ["fiesta": ["year": "2001", "year": "2002"], "focus": ["year": "2001","year":  "2002"]], ["opel": ["astra": ["year": "2001","year":  "2002"]], "corsa": ["year": "2001","year": "2002"]]]
-    var carDictionary = ["ford": ["fiesta": ["year": "2001", "year": "2002"], "focus": ["year": "2001","year": "2002"]], "ford": ["fiesta": ["year": "2001", "year": "2002"], "focus": ["year": "2001","year": "2002"]]]
+    var listOfModels: [String] = [String]()
+    var listOfYears: [String] = [String]()
     
-    //var carList: Dictionary <String, Dictionary<String,Dictionary<String,String>>> = Dictionary()
-    //{ make1: {model1: {year1, year2}, model2: {year1, year2}}, make2: {model4: {year16, year23}, model5: {year21, year23}}}
+    //dla tych danych pierwszy poziom to jest key
+    //dictionary ma być brane z serwera -> skrypt w Pythonie robiący nested dictionaries z pliku json/csv
+    //przenieść ten cały carDictionary do osobnej lokalizacji
+    
+    var carDictionary = ["car": ["fiesta": ["year": ["1990", "2002"]], "focus": ["year": ["2020", "2002"]]], "car2": ["reno": ["year": ["1992", "2002"]], "super": ["year": ["1993", "1994"]]]]
     
     var makeRow = 0
     var modelRow = 0
     var yearRow = 0
     
-    //var carDictionary = ["ford": ["fiesta": ["year": "2001", "year": "2002"], "focus": ["year": "2001","year":  "2002"]], ["opel": ["astra": ["year": "2001","year":  "2002"], "corsa": ["year": "2001","year": "2002"]]]
-    
     override func viewDidLoad() {
+        //keys - "car" z przykładu, dictionary z pierwszego poziomiu
         self.listOfMakes = Array(carDictionary.keys)
         let firstCar = listOfMakes[0]
-        let modelsDictionary = carList[firstCar]! as  Dictionary<String, Dictionary<String, String>>
-        //self
-        var listOfModels = Array(modelsDictionary.keys).sort()
+        let modelsDictionary = carDictionary[firstCar]! as  Dictionary<String, Dictionary<String, [String]>>
+        var listOfModels = Array(modelsDictionary.keys).sorted()
         let firstModel = listOfModels[0]
-        let yearsDictionary = modelsDictionary[firstModel]! as Dictionary<String, String>
+        let yearsDictionary = modelsDictionary[firstModel]! as Dictionary<String, [String]>
         //self
-        var listOfYears = Array(yearsDictionary.keys).sort()
+        var listOfYears = Array(yearsDictionary.keys).sorted()
+        
+        makePicker.dataSource = self
+        makePicker.delegate = self
+        modelPicker.dataSource = self
+        modelPicker.delegate = self
+        yearPicker.dataSource = self
+        yearPicker.delegate = self
     }
-    
     
     func setModelsList(make: String) {
-        let modelsDictionary = carList[make]! as  Dictionary<String, Dictionary<String, String>>
-        self.listOfModels = Array(modelsDictionary.keys).sort()
+        let modelsDictionary = carDictionary[make]! as  Dictionary<String, Dictionary<String, [String]>>
+        self.listOfModels = Array(modelsDictionary.keys).sorted()
     }
     func setYearsList(make: String, model: String) {
-        let modelsDictionary = carList[make]! as  Dictionary<String, Dictionary<String, String>>
-        let modelsList = Array(modelsDictionary.keys)
-        let yearsDictionary = modelsDictionary[model]! as Dictionary<String, String>
-        self.listOfYears = Array(yearsDictionary.keys).sort()
+        let modelsDictionary = carDictionary[make]! as  Dictionary<String, Dictionary<String, [String]>>
+        //let modelsList = Array(modelsDictionary.keys)
+        let yearsArray = modelsDictionary[model]! as Dictionary<String, [String]>
+        if let years = yearsArray["year"] {
+            self.listOfYears = years
+        }
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -64,26 +68,19 @@ class CarPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerV
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         
         if pickerView == makePicker {
-            makeRow = row
-            setModelsList(listOfMakes[makeRow])
-            setYearsList(listOfMakes[makeRow], model: listOfModels[0])
+            setModelsList(make: listOfMakes[makeRow])
             modelPicker.selectRow(0, inComponent: 0, animated: true)
-            yearPicker.selectRow(0, inComponent: 0, animated: true)
-            self.modelPicker.reloadAllComponents()
-            self.yearPicker.reloadAllComponents()
+            return listOfMakes.count
         }
         else if pickerView == modelPicker {
-            modelRow = row
-            setYearsList(listOfMakes[makeRow], model: listOfModels[modelRow])
-            yearPicker.selectRow(0, inComponent: 0, animated: true)
-            self.modelPicker.reloadAllComponents()
-            self.yearPicker.reloadAllComponents()
+            return listOfModels.count
         }
         else {
-            //do anything you want once you have chosen a specific year.
+            return listOfYears.count
         }
+        
     }
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if pickerView == makePicker {
             return listOfMakes[row]
         }
@@ -94,5 +91,32 @@ class CarPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerV
             return listOfYears[row]
         }
     }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        if pickerView == makePicker {
+            makeRow = row
+            setModelsList(make: listOfMakes[makeRow])
+            setYearsList(make: listOfMakes[makeRow], model: listOfModels[0])
+            modelPicker.selectRow(0, inComponent: 0, animated: true)
+            yearPicker.selectRow(0, inComponent: 0, animated: true)
+            self.modelPicker.reloadAllComponents()
+            self.yearPicker.reloadAllComponents()
+            
+            
+        }
+        else if pickerView == modelPicker {
+            modelRow = row
+            setYearsList(make: listOfMakes[makeRow], model: listOfModels[modelRow])
+            yearPicker.selectRow(0, inComponent: 0, animated: true)
+            self.yearPicker.reloadAllComponents()
+        }
+        else {
+            //delegat? do drugiego vc, połączone do jednego stringa
+            let pickerString = "\(listOfMakes[row]) \(listOfModels[row]) \(listOfYears[row])"
+            print(pickerString)
+        }
+        
+    }
+    
 }
-
